5eb1451719cc5cadd35bcbf6828f7eaeffa190be
N34AY <dm.penzyakov@gmail.com>
2021-07-01 20:53:41 +0200

    author parameter when init

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const { default: axios } = require("axios")
 
 class Logger {
-  constructor(token) {
+  constructor(token, author) {
     this.token = token
     this.baseUrl = 'http://localhost:3014'
+    this.author = author
   }
 
   async sendLog(options) {
+    if (!options.author) options.author = this.author
     const response = await axios.post(this.baseUrl + `/logs/add/${this.token}`, options)
     return response.data
   }
 
   async getLogs(options) {
+    if (!options.author) options.author = this.author
     const response = await axios.post(this.baseUrl + `/logs/get/${this.token}`, options)
     return response.data
   }
