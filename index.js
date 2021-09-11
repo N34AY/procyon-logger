1b8ddac96ca85588a20eb843741314003e980557
N34AY <dm.penzyakov@gmail.com>
2021-09-11 21:25:52 +0300

    send param

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ class Logger {
   }
 
   async sendLog(options) {
-    if (!options.author) options.author = this.author
-    if (send) {
+    try {
+      if (!options.author) options.author = this.author
+      if (!this.send && options.send) console.warn('\x1b[33m', 'WARNING: Constructor takes "send: false" but you pass "send: true" in sendLog method. Log will not be sent.', '\x1b[0m')
+      if (!this.send || options.send === false) return console.log('logger - sendLog: ', options)
       const response = await axios.post(this.baseUrl + `/logs/add/${this.token}`, options)
       return response.data
-    } else console.log('logger - sendLog: ', options)
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   async getLogs(options) {
--- test.js
+++ test.js
@@ -1,13 +1,12 @@
 const Logger = require('./index')
 
+const logger = new Logger('some-key', 'test', false)
 
-const logger = new Logger('some-key')
-
-logger.sendLog({ author: 'test author', message: 'test msg', type: 'error', data: { error: 'test error' } })
+logger.sendLog({ author: 'test author', message: 'test msg', type: 'error', data: { error: 'test error' }, send: true })
 
 async function getLogs() {
   const logs = await logger.getLogs({ author: 'test author', type: 'error' })
   console.log(logs)
 }
 
-getLogs()
+// getLogs()
