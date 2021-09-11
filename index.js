014829f71d99f092bb832fa37e25a1dd78514e7d
N34AY <dm.penzyakov@gmail.com>
2021-09-11 20:22:25 +0300

    send status

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const { default: axios } = require("axios")
 
 class Logger {
-  constructor(token, author) {
+  constructor(token, author, send) {
     this.token = token
     this.baseUrl = 'http://localhost:3014'
     this.author = author
+    this.send = send
   }
 
   async sendLog(options) {
     if (!options.author) options.author = this.author
-    const response = await axios.post(this.baseUrl + `/logs/add/${this.token}`, options)
-    return response.data
+    if (send) {
+      const response = await axios.post(this.baseUrl + `/logs/add/${this.token}`, options)
+      return response.data
+    } else console.log('logger - sendLog: ', options)
   }
 
   async getLogs(options) {
